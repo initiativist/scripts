d1d45a48908d7e81bebac4ac3dba3f25b98ca376
Cabbage Stone <caleb@theradicalself.com>
2022-10-22 19:18:33 -0600

    finish go insert implementation

--- data_alg/implementations/array/go_array.go
+++ data_alg/implementations/array/go_array.go
@@ -24,51 +24,36 @@ func (myArray MyArray) traverse() {
 	}
 }
 
-func (myArray MyArray) insert(value string, index int) {
+func add_custom_array_space(smallerArray []string, amount int) []string {
+	largerArray := make([]string, len(smallerArray), cap(smallerArray)+amount)
+	copy(largerArray, smallerArray)
+	largerArray = largerArray[0 : len(smallerArray)+amount]
+	smallerArray = largerArray
+	return smallerArray
+}
+
+func (myArray MyArray) insert(value string, index int) MyArray {
 	if index > myArray.length() || index < 0 {
-		return
+		panic("Index must be appropriate for array")
 	}
 
 	reverseIterator := myArray.length()
-	myArray.vals = append(myArray.vals, "")
+	myArray.vals = add_custom_array_space(myArray.vals, 1)
 	for reverseIterator >= index && reverseIterator > 0 {
 		myArray.vals[reverseIterator] = myArray.vals[reverseIterator-1]
 		reverseIterator = reverseIterator - 1
 	}
 
 	myArray.vals[index] = value
+	return myArray
 }
 
 func (myArray MyArray) length() int {
 	return len(myArray.vals)
 }
 
-func split_string_on_character(value string, splitStr string) []string {
-	result := make([]string, 1, 2)
-	if len(value) <= 1 {
-		return result
-	}
-	if len(splitStr) > 1 {
-		panic("splitStr must be a single character")
-	}
-
-	for i := 0; i < len(value); i++ {
-		if value[i] == splitStr[0] {
-			reallocatedResult := make([]string, len(result), cap(result)+1)
-			copy(reallocatedResult, result)
-			reallocatedResult = reallocatedResult[0 : len(result)+1]
-			result = reallocatedResult
-			continue
-		}
-		result[len(result)-1] = result[len(result)-1] + string(value[i])
-	}
-	return result
-}
-
 func main() {
 	myArray := MyArray{os.Args[1:]}
-	myArray.traverse()
-	fmt.Print(len(split_string_on_character("1 1", " ")))
 
 	for {
 		fmt.Print("\nlist perform: ")
@@ -88,23 +73,20 @@ func main() {
 			break
 		case "i":
 			fmt.Print("\n($element $array): ")
-			var insertCommand string
-			fmt.Scan(&insertCommand)
-			argList := split_string_on_character(insertCommand, " ")
+			var insertValue string
+			var insertIndex string
 
-			if len(argList) != 2 {
+			var _, scanErr = fmt.Scan(&insertValue, &insertIndex)
+			if scanErr != nil {
 				fmt.Print("\n! INCORRECT FORMAT\n ")
 			}
 
-			indexStr := argList[1]
-			value := argList[0]
-
-			index, err := strconv.Atoi(indexStr)
+			index, err := strconv.Atoi(insertIndex)
 			if err != nil || index > myArray.length() {
 				fmt.Print("\n! UNKNOWN INDEX \n")
 			}
 
-			myArray.insert(value, index)
+			myArray = myArray.insert(insertValue, index)
 			break
 
 		case "x":
