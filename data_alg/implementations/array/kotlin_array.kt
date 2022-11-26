9eb2fb6f7fb8333bda2521fae0c884a2b2b30181
Cabbage Stone <caleb@theradicalself.com>
2022-11-26 14:07:48 -0700

    kotlin initial

--- data_alg/implementations/array/kotlin_array.kt
+++ data_alg/implementations/array/kotlin_array.kt
@@ -1,5 +1,25 @@
 //kotlinc ./kotlin_array.kt -include-runtime -d compiled/kotlin_array.jar
 
 fun main(args : Array<String>) {
-    println("Hello, World!")
+    val myArray = MyArray(args);
+
+    mainProcess@ while (true) {
+        print("list perform: ");
+        val command = readLine();
+        when (command) {
+            "h" -> println("x: exit\nt: traverse\ni: insert\n");
+            "t" -> myArray.traverse();
+            "x" -> break@mainProcess
+        }
+    }
+}
+
+class MyArray (private val _myArray: Array<String>) {
+    constructor() : this(arrayOf<String>());
+
+    fun traverse() {
+        for ((index, value) in this.`_myArray`.withIndex()) {
+            println("list[$index] = $value");
+        }
+    }
 }
