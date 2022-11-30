40b48c736dca5e1bc8f85dcc56c717560c18959c
Cabbage Stone <caleb@theradicalself.com>
2022-11-29 18:23:36 -0700

    swift control loop implementation

--- data_alg/implementations/array/swift_array.swift
+++ data_alg/implementations/array/swift_array.swift
@@ -1,7 +1,26 @@
 func main() -> Void {
 	let myArray = MyArray(fromArray: Array(CommandLine.arguments[1...]))
-	myArray.traverse()
 	
+	process: while true {
+		print("list perform: ", terminator: "")
+		let command = readLine(strippingNewline: true)
+
+		if command?.count != 1 {
+			print("\n! Unknown Command! \n")
+			continue
+		}
+
+		switch command {
+			case "h":
+				print("\nx: exit\nt: traverse\ni: insert")
+			case "x":
+				break process
+			case "t":
+				myArray.traverse()
+			default:
+				print("\n! UNKNOWN COMMAND! \n")
+		}
+	}
 }
 
 struct MyArray {
